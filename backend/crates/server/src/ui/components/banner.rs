e6504b04e8ce5971c13cd7b5fba124f919fbdaf0
confused-moniker <ralph@iguhealth.app>
2026-05-07 17:01:21 -0500

    modify interaction comp server

--- backend/crates/server/src/ui/components/banner.rs
+++ backend/crates/server/src/ui/components/banner.rs
@@ -5,10 +5,10 @@ use crate::static_assets::asset_route;
 pub fn banner(header: &str, subheader: Option<&str>) -> Markup {
     html! {
         div class="flex flex-col items-center justify-center space-y-1" {
-            a href="#" class="h-20 w-50 relative flex justify-center items-center text-2xl font-semibold text-gray-900" {
+            a href="#" class="h-20 w-54 relative flex justify-center items-center text-2xl font-semibold text-gray-900" {
                 img src=(asset_route("img/logo_text.svg")) alt="logo"  {}
             }
-            div class="flex space-x-1 items-center justify-center text-sm text-slate-400" {
+            div class="flex space-x-1 items-center justify-center text-sm text-slate-400 mt-[-12px]" {
                 div {
                     span class="font-bold" {
                         (header)
