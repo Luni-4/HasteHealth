aab4d077a45f1be07c8ecf993f015bcbf8df0801
RP <93403502+confused-moniker@users.noreply.github.com>
2026-05-27 17:26:17 -0500

    Split line scripts (#702)
    * mod
    
    * mod

--- backend/scripts/testscript_build.sh
+++ backend/scripts/testscript_build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-cargo run generate test-scripts -i ./crates/artifacts/artifacts/r4/test_data/us_core -o ./testscripts/us-core/generated
-cargo run generate test-scripts -i ./crates/artifacts/artifacts/r4/test_data/r4_r5_backport -o ./testscripts/r4-r5-backport/generated
+cargo run generate test-scripts \
+    -i ./crates/artifacts/artifacts/r4/test_data/us_core \
+    -o ./testscripts/us-core/generated
+cargo run generate test-scripts \
+    -i ./crates/artifacts/artifacts/r4/test_data/r4_r5_backport \
+    -o ./testscripts/r4-r5-backport/generated
--- backend/scripts/types_build.sh
+++ backend/scripts/types_build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-cargo run generate types -i ./crates/artifacts/artifacts/r4/haste_health/structure_definition -i ./crates/artifacts/artifacts/r4/haste_health/terminology -i ./crates/artifacts/artifacts/r4/hl7/original/profiles-types.json -i ./crates/artifacts/artifacts/r4/hl7/original/profiles-resources.json -i ./crates/artifacts/artifacts/r4/hl7/original/valuesets.json -i ./crates/artifacts/artifacts/r4/hl7/original/v3-codesystems.json -o ./crates/fhir-model/src/r4/generated
+cargo run generate types \
+    -i ./crates/artifacts/artifacts/r4/haste_health/structure_definition \
+    -i ./crates/artifacts/artifacts/r4/haste_health/terminology \
+    -i ./crates/artifacts/artifacts/r4/hl7/original/profiles-types.json \
+    -i ./crates/artifacts/artifacts/r4/hl7/original/profiles-resources.json \
+    -i ./crates/artifacts/artifacts/r4/hl7/original/valuesets.json \
+    -i ./crates/artifacts/artifacts/r4/hl7/original/v3-codesystems.json \
+    -o ./crates/fhir-model/src/r4/generated
