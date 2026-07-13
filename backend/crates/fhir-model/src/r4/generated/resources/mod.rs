3700b3adfaee18a3913138957fea25674ea818ea
Luni-4 <luni-4@hotmail.it>
2026-07-13 20:45:14 +0200

    wip

---
+++ backend/crates/fhir-model/src/r4/generated/resources/mod.rs
@@ -0,0 +1,30 @@
+#![allow(non_snake_case)]
+
+mod resources_000;
+mod resources_001;
+mod resources_002;
+mod resources_003;
+mod resources_004;
+mod resources_005;
+mod resources_006;
+mod resources_007;
+mod resources_008;
+mod resources_009;
+mod resources_010;
+mod resources_011;
+mod resources_012;
+
+// Re-export everything to the higher-level crate interface
+pub use resources_000::*;
+pub use resources_001::*;
+pub use resources_002::*;
+pub use resources_003::*;
+pub use resources_004::*;
+pub use resources_005::*;
+pub use resources_006::*;
+pub use resources_007::*;
+pub use resources_008::*;
+pub use resources_009::*;
+pub use resources_010::*;
+pub use resources_011::*;
+pub use resources_012::*;
